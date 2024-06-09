26e2df3076a322b20852193bd52690bda7d1233e
AnaCarolinaCCH123 <A00838713@tec.mx>
2024-06-09 15:14:25 -0600

    Update Truck.cpp

--- Truck.cpp
+++ Truck.cpp
@@ -1,25 +1,25 @@
-#include "Trucks.h"
+#include "Truck.h"
 #include <iostream>
 
-Trucks::Trucks(int year, string manufacturer, int speed, int Wheels, string name)
+Truck::Truck(int year, string manufacturer, int speed, int Wheels, string name)
     : Vehicle (manufacturer, speed, year, name) {
     this->Wheels= Wheels;
 }
 
-int  Trucks::getWheels() {
+int  Truck::getWheels() {
     return Wheels;
 }
 
 
-void Trucks::setWheels(int Wheels) {
+void Truck::setWheels(int Wheels) {
     this->Wheels = Wheels;
 }
 
-string Trucks:: VehicleInfo (string ExtraInfo) {
+string Truck:: VehicleInfo (string ExtraInfo) {
     return VehicleInfo() + " " + ExtraInfo + " Has approximately : " + to_string(Wheels) + " wheels";
 }
 
-string Trucks::VehicleInfo()
+string Truck::VehicleInfo()
 {
 return "Trucking is a male-dominated industry with only 6% of all commercial truckers being women"; 
     
